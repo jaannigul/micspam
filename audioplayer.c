f6bc5b2f4db2db0fe99ded05e66300060b9b3987
SparklyCatTF2 <32608971+madiskoivopuu@users.noreply.github.com>
2024-01-10 17:32:29 +0200

    Fix bug with only the last stream being associated with rtaudio_t

--- audioplayer.c
+++ audioplayer.c
@@ -8,11 +8,18 @@
 
 #define BUFFER_FRAMES 256
 
-rtaudio_t audioInstance = 0;
+rtaudio_t realDeviceAudio = 0;
+rtaudio_t virtualDeviceAudio = 0;
 
-int micInputCallback(void* out, void* in, unsigned int nFrames,
+int realMicAndHeadphonesCallback(void* out, void* in, unsigned int nFrames,
     double stream_time, rtaudio_stream_status_t status,
     void* userdata) {
+
+    if (out)
+        printf("a\n");
+
+    if (in)
+        printf("b\n");
 
     return 0;
 }
@@ -21,19 +28,16 @@ int virtualMicCallback(void* out, void* in, unsigned int nFrames,
     double stream_time, rtaudio_stream_status_t status,
     void* userdata) {
 
+    if(out)
+        printf("c\n");
+
     return 0;
 }
 
-int headphoneAudioCallback(void* out, void* in, unsigned int nFrames,
-    double stream_time, rtaudio_stream_status_t status,
-    void* userdata) {
-
-}
-
 void listDevices() {
-    int devices = rtaudio_device_count(audioInstance);
+    int devices = rtaudio_device_count(realDeviceAudio);
     for (int i = 0; i < devices; i++) {
-        rtaudio_device_info_t deviceInfo = rtaudio_get_device_info(audioInstance, i);
+        rtaudio_device_info_t deviceInfo = rtaudio_get_device_info(realDeviceAudio, i);
         
         printf("[%d]: %s, input channels: %d, output channels: %d\n", i, deviceInfo.name, deviceInfo.input_channels, deviceInfo.output_channels);
     }
@@ -54,51 +58,60 @@ _Bool selectMicAndAudioDevices() {
     int virtualMicId = atoi(virtualMicSelectedId);
     int headphonesId = atoi(headphonesSelectedId);
 
-    rtaudio_device_info_t realMicInfo = rtaudio_get_device_info(audioInstance, micId);
+    rtaudio_device_info_t realMicInfo = rtaudio_get_device_info(realDeviceAudio, micId);
     rtaudio_stream_parameters_t realMicParams = { 0 };
     realMicParams.device_id = micId;
     realMicParams.num_channels = realMicInfo.input_channels;
 
-    int prefFrames = BUFFER_FRAMES;
-    int err = rtaudio_open_stream(audioInstance, NULL, &realMicParams, RTAUDIO_FORMAT_SINT16, realMicInfo.preferred_sample_rate, &prefFrames, &micInputCallback, NULL, NULL, NULL);
-    if (err != RTAUDIO_NO_ERROR) {
-        printf("Failed to open microphone device with id %d due to error: %s\n", micId, rtaudio_error(audioInstance));
-        return TRUE;
-    }
-
-    rtaudio_device_info_t virtualMicInfo = rtaudio_get_device_info(audioInstance, virtualMicId);
+    rtaudio_device_info_t virtualMicInfo = rtaudio_get_device_info(virtualDeviceAudio, virtualMicId);
     rtaudio_stream_parameters_t virtualMicParams = { 0 };
     virtualMicParams.device_id = virtualMicId;
     virtualMicParams.num_channels = virtualMicInfo.output_channels;
 
-    err = rtaudio_open_stream(audioInstance, &virtualMicParams, NULL, RTAUDIO_FORMAT_SINT16, virtualMicInfo.preferred_sample_rate, &prefFrames, &virtualMicCallback, NULL, NULL, NULL);
-    if (err != RTAUDIO_NO_ERROR) {
-        printf("Failed to open virtual microphone device with id %d due to error: %s\n", virtualMicId, rtaudio_error(audioInstance));
-        return TRUE;
-    }
-
-    rtaudio_device_info_t headphonesInfo = rtaudio_get_device_info(audioInstance, headphonesId);
+    rtaudio_device_info_t headphonesInfo = rtaudio_get_device_info(realDeviceAudio, headphonesId);
     rtaudio_stream_parameters_t headphoneParams = { 0 };
     headphoneParams.device_id = headphonesId;
     headphoneParams.num_channels = headphonesInfo.output_channels;
 
-    err = rtaudio_open_stream(audioInstance, &headphoneParams, NULL, RTAUDIO_FORMAT_SINT16, headphonesInfo.preferred_sample_rate, &prefFrames, &headphoneAudioCallback, NULL, NULL, NULL);
-    if (err != RTAUDIO_NO_ERROR) {
-        printf("Failed to open headphone device with id %d due to error: %s\n", virtualMicId, rtaudio_error(audioInstance));
+    int prefFrames = BUFFER_FRAMES;
+    int err = rtaudio_open_stream(realDeviceAudio, &headphoneParams, &realMicParams, RTAUDIO_FORMAT_SINT16, realMicInfo.preferred_sample_rate, &prefFrames, &realMicAndHeadphonesCallback, NULL, NULL, NULL);
+    if (err > RTAUDIO_ERROR_DEBUG_WARNING) {
+        printf("Failed to open microphone or headphone device due to error: %s\n", micId, rtaudio_error(realDeviceAudio));
+        return FALSE;
+    }
+
+    err = rtaudio_open_stream(virtualDeviceAudio, &virtualMicParams, NULL, RTAUDIO_FORMAT_SINT16, virtualMicInfo.preferred_sample_rate, &prefFrames, &virtualMicCallback, NULL, NULL, NULL);
+    if (err > RTAUDIO_ERROR_DEBUG_WARNING) {
+        printf("Failed to open virtual microphone device with id %d due to error: %s\n", virtualMicId, rtaudio_error(virtualDeviceAudio));
         return TRUE;
     }
+
+    return TRUE;
 }
 
 int main() {
-    audioInstance = rtaudio_create(RTAUDIO_API_WINDOWS_WASAPI);
-    if (!audioInstance) {
-        const char* error = rtaudio_error(audioInstance);
-        printf("Error creating RtAudio instance: %s\n", error);
+    realDeviceAudio = rtaudio_create(RTAUDIO_API_WINDOWS_WASAPI);
+    if (!realDeviceAudio) {
+        printf("Error creating RtAudio instance for physical devices: %s\n", rtaudio_error(realDeviceAudio));
+
+        return 1;
+    }
+
+    virtualDeviceAudio = rtaudio_create(RTAUDIO_API_WINDOWS_WASAPI);
+    if (!virtualDeviceAudio) {
+        printf("Error creating RtAudio instance for virtual devices: %s\n", rtaudio_error(virtualDeviceAudio));
 
         return 1;
     }
 
     if (!selectMicAndAudioDevices()) return 1;
+
+    rtaudio_start_stream(realDeviceAudio);
+    rtaudio_start_stream(virtualDeviceAudio);
+
+    while (1) {
+        Sleep(1);
+    }
 
     return 0;
 }
