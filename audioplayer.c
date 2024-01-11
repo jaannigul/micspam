d934f0e5e87efad1336ca10024ee2b5f4f4815c0
SparklyCatTF2 <32608971+madiskoivopuu@users.noreply.github.com>
2024-01-11 17:49:06 +0200

    Create a function for counting audio files and checking file extension

--- audioplayer.c
+++ audioplayer.c
@@ -1,4 +1,6 @@
 #include "audioplayer.h"
+#include "consts.h"
+
 #include <Windows.h>
 
 pthread_t soundPlayer;
@@ -12,9 +14,34 @@ _Bool setupAudioPlayer() {
 	return TRUE;
 }
 
-// count how many audio files user has in a specific folder
-int countUserAudioFiles(const char* path) {
+_Bool checkForSuffix(const char* str, const char* suffix)
+{
+	if (!str || !suffix)
+		return 0;
+	size_t lenstr = strlen(str);
+	size_t lensuffix = strlen(suffix);
+	if (lensuffix > lenstr)
+		return 0;
+	return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
 
+// count how many audio files user has in a specific folder
+int countFilesInDirectory(const char* path) {
+	WIN32_FIND_DATA fileData;
+	int filesTotal = 0;
+
+	HANDLE hFile = FindFirstFile(TEXT(USER_AUDIO_FILES_PATH), &fileData);
+	if (hFile == INVALID_HANDLE_VALUE)
+		return filesTotal;
+
+	if (GetFileAttributes(fileData.cFileName) & FILE_ATTRIBUTE_DIRECTORY == 0)
+		filesTotal++;
+
+	while (FindNextFile(hFile, &fileData) != 0)
+		if (GetFileAttributes(fileData.cFileName) & FILE_ATTRIBUTE_DIRECTORY == 0)
+			filesTotal++;
+
+	return filesTotal;
 }
 
 const char** getUserAudioFiles(const char* path) {
